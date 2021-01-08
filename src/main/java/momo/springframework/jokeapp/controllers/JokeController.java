a286dd45758d4440d480d408791019e077f4aae8
NeMoMo <piotr@theworlt.com>
2021-01-08 16:53:15 +0100

    Add controller and view

---
+++ src/main/java/momo/springframework/jokeapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package momo.springframework.jokeapp.controllers;
+
+import momo.springframework.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
